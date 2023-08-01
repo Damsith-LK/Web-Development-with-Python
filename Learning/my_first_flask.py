62f3bc55dbb7c6ff279e0ab1a693fe114f243ff1
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-01 12:20:46 +0530

    Learn about variables in Flask and debug mode

--- Learning/my_first_flask.py
+++ Learning/my_first_flask.py
@@ -14,6 +14,18 @@ def bye():
     """This is gonna get triggerd if the user adds '/bye' into the end of the URL and hit enter"""
     return "Bye!"
 
+# We can use variables in this <way>
+@app.route("/user/<username>")
+def greeting(username):
+    """The webpage would show whatever the username that was typed in after '/user/' """
+    return f"What's going on, {username}?"
+
+
+# Also, we can specify the data type of the variable
+@app.route("/user/<username>/<int:number>")
+def lucky_number(username, number):
+    return f"{username}, your lucky number is {number}"
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)  # If debug=True, we will be able to debug the webpage we created live
