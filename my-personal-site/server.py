c394580214a62b8d465527293cd064cbe6471e11
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-05 17:05:05 +0530

    Complete Day 56 The complex code in here is not mine though I got a template from internet and modified it

--- my-personal-site/server.py
+++ my-personal-site/server.py
@@ -1,6 +1,8 @@
 # Day 56
-# Here I'm going to make a personal website using Flask (gonna use html, css too)
-# A separate folder named 'templates' is where the html, css files should go
+# Here I'm going to make a personal website (a name card) using Flask (gonna use html, css too)
+# A separate folder named 'templates' is where the html files should go
+# And a folder named 'static' is where static files such as css and images should go
+# Using a website template here
 # Note: This is a learning project
 
 from flask import Flask, render_template
@@ -8,7 +10,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home_page():
     return render_template("index.html")
 
 if __name__ == "__main__":
