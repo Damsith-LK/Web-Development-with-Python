152d37f143eef717cd4faa88ec2d02bdd7366484
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-06 11:38:31 +0530

    Learn about multiline statements in Jinja

--- Jinja/server.py
+++ Jinja/server.py
@@ -25,5 +25,13 @@ def guess(name):
     return render_template("guess.html", name=name.capitalize(), age=age, gender=gender, year=year)
 
 
+@app.route("/blog")
+def blog():
+    """Blog webpage (uses jinja multiline statements)"""
+    blog_response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")  # Getting random blogs
+    blogs = blog_response.json()
+    return render_template("blog.html", blogs=blogs)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
