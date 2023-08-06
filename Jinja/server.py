1fcaf84870648e51e521c847e07b410f5558e8f8
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-06 11:12:47 +0530

    Complete challenge 1 of Day 57 - Gender and Age guessing website

--- Jinja/server.py
+++ Jinja/server.py
@@ -1,15 +1,29 @@
 # Day 57 - Templating with Jinja
+# Challenge 1 is also here - Gender and Age guessing website
 
 from flask import Flask, render_template
 import random
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
+year = datetime.now().year
 
 @app.route("/")
 def home_page():
     # In order to show a random number, we'll need to use kwargs of render_template()
-    return render_template("index.html", rand_num=random.randint(1, 10), year=datetime.now().year)
+    return render_template("index.html", rand_num=random.randint(1, 10), year=year)
+
+
+@app.route("/guess/<name>")
+def guess(name):
+    """Displays the guessed age and gender of a given name"""
+    age_response = requests.get(f"https://api.agify.io?name={name}")
+    gender_response = requests.get(f"https://api.genderize.io?name={name}")
+    age = age_response.json()["age"]
+    gender = gender_response.json()["gender"]
+    return render_template("guess.html", name=name.capitalize(), age=age, gender=gender, year=year)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
