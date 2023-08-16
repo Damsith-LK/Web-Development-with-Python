989e8fd03cb4bef939bc31a470ee21d8acffde31
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-16 19:13:41 +0530

    Start Day 59 - Upgrading Blog

--- Blog/main.py
+++ Blog/main.py
@@ -1,5 +1,5 @@
 # Day 57 - Blog Capstone project
-# Successfully completed it
+# Day 59 - Upgrading Blog
 
 from flask import Flask, render_template
 from post import Post
@@ -12,12 +12,12 @@ blogs = post.get_blogs()
 def home():
     return render_template("index.html", blogs=blogs)
 
-@app.route("/post/<int:num>")
-def post(num):
-    title = blogs[num-1]["title"]
-    body = blogs[num-1]["body"]
-    subtitle = blogs[num-1]["subtitle"]
-    return render_template("post.html", title=title, body=body, subtitle=subtitle)
+# @app.route("/post/<int:num>")
+# def post(num):
+#     title = blogs[num-1]["title"]
+#     body = blogs[num-1]["body"]
+#     subtitle = blogs[num-1]["subtitle"]
+#     return render_template("post.html", title=title, body=body, subtitle=subtitle)
 
 
 if __name__ == "__main__":
