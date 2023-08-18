4f04b66b9318a5ba4045467d38e69c0da2f7d55e
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-18 17:56:08 +0530

    Get the HTML Forms to work and create a login page to show the submitted values

--- HTML-forms/main.py
+++ HTML-forms/main.py
@@ -1,12 +1,19 @@
 # Day 60 - Learning about HTML forms
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    """Getting the data entered into HTML Form"""
+    name = request.form["name"]
+    password = request.form["password"]
+    return render_template("login.html", name=name, password=password)
 
 if __name__ == "__main__":
     app.run(debug=True)
