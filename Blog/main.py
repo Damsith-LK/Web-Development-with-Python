14e3e57f8f17a6863a660b6ee1e2c41386c47eb1
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-18 11:20:09 +0530

    Build urls for about/contact pages and add styling to them

--- Blog/main.py
+++ Blog/main.py
@@ -10,7 +10,7 @@ blogs = post.get_blogs()
 
 @app.route('/')
 def home():
-    return render_template("index.html", blogs=blogs)
+    return render_template("index.html")
 
 # @app.route("/post/<int:num>")
 # def post(num):
@@ -18,6 +18,14 @@ def home():
 #     body = blogs[num-1]["body"]
 #     subtitle = blogs[num-1]["subtitle"]
 #     return render_template("post.html", title=title, body=body, subtitle=subtitle)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
