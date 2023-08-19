7065de9659c459e473d0316c5e3db51cbbad65db
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-19 16:56:08 +0530

    Create a super-simple WTForm

--- WTForms/main.py
+++ WTForms/main.py
@@ -1,8 +1,16 @@
 # Day 61 - Learning about WTForms
 
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
 
 app = Flask(__name__)
+app.secret_key = "wakeuptoreality"
+
+class MyForm(FlaskForm):
+    """Creating a WTF class with inheritance"""
+    email = StringField('Email')
+    password = StringField('Password')
 
 @app.route("/")
 def home():
@@ -10,7 +18,8 @@ def home():
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    form = MyForm()
+    return render_template("login.html", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
