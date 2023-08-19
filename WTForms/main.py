faf1ce6ea681f75b9475a64ab72aaaff8c6ccbea
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-19 17:41:33 +0530

    Add validators in WTForms and show errors in them

--- WTForms/main.py
+++ WTForms/main.py
@@ -3,24 +3,26 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, NoneOf
 
 app = Flask(__name__)
 app.secret_key = "wakeuptoreality"
 
-class MyForm(FlaskForm):
+class LoginForm(FlaskForm):
     """Creating a WTF class with inheritance"""
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
+    email = StringField(label='Email', validators=[DataRequired(), Email(), NoneOf(values=".")])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Submit")
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    form = MyForm()
-    return render_template("login.html", form=form)
+    login_form = LoginForm()
+    login_form.validate_on_submit()
+    return render_template("login.html", form=login_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
