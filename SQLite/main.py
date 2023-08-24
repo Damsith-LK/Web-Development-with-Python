84025a71c2e18ab8a3681c43a93e768dba98a2f0
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-24 13:05:19 +0530

    Create an entry in DB

--- SQLite/main.py
+++ SQLite/main.py
@@ -8,4 +8,9 @@ db = sqlite3.connect("books-database.db")
 
 # Creating a cursor which will control our database
 cursor = db.cursor()
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# Error will happen if I try to create the table again
+
+# Remember to close Database on DB Browser before creating new entries
+cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+db.commit()
