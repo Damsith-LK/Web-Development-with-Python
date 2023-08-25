3a509aaf5afebe3b5d20ffff983b68bc025e29bd
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-25 17:04:39 +0530

    Complete the 1st requirement of the website - View movie list items
    Storing these movie values in SQL DB and program reads the in order to display them on website

--- Favourite-Movies/main.py
+++ Favourite-Movies/main.py
@@ -1,4 +1,12 @@
 # Day 64 - My Top 10 Favourite Movies Website
+"""
+Requirements of this website
+    1. Be Able to View Movie List Items
+    2. Be Able to Edit a Movie's Rating and Review
+    3. Be Able to Delete Movies from the Database
+    4. Be Able to Add New Movies Via the Add Page
+    5. Be Able to Sort and Rank the Movies By Rating
+"""
 
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
@@ -9,13 +17,33 @@ from wtforms.validators import DataRequired
 import requests
 
 app = Flask(__name__)
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///top-movies-database.db"
+db.init_app(app)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+
+# Create table
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+
+# Schema
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    with app.app_context():
+        all_movies = db.session.execute(db.select(Movie).order_by(Movie.title)).scalars().all()
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
