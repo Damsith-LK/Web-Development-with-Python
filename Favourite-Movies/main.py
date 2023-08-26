9f6195d338eba6fde77c0d8d9295a6f93f045f72
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-08-26 18:27:25 +0530

    Create the first part of movie-adding mechanism
    For this I use The Movies Database (TMDB) API

--- Favourite-Movies/main.py
+++ Favourite-Movies/main.py
@@ -7,6 +7,7 @@ Requirements of this website
     4. Be Able to Add New Movies Via the Add Page
     5. Be Able to Sort and Rank the Movies By Rating
 """
+# Don't confuse 'requests' for API get and 'request' in flask
 
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
@@ -14,6 +15,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, FloatField
 from wtforms.validators import DataRequired, Length, NumberRange
+import config
 import requests
 
 app = Flask(__name__)
@@ -22,6 +24,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///top-movies-database.db"
 db.init_app(app)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+# Headers for TMDB requesting
+tmdb_headers = {"accept": "application/json", "Authorization": config.tmdb_key}
 
 # Create table
 class Movie(db.Model):
@@ -43,6 +47,20 @@ class RateMovieForm(FlaskForm):
     rating = FloatField(label="Your rating out of 10, eg.7.5", validators=[DataRequired(), NumberRange(min=0, max=10)])
     review = StringField(label="Your Review", validators=[DataRequired(), Length(max=40)])
     submit = SubmitField(label="Done")
+
+# Form to add new movies into the database
+class AddMovieForm(FlaskForm):
+    title = StringField(label="Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
+
+
+def get_movie_info(title) -> list:
+    """This function is for getting the relevant information about movies according to the given input from TMDB API.
+    Use this inside add() function"""
+    api_url = "https://api.themoviedb.org/3/search/movie"
+    params = {"query": title}
+    response = requests.get(api_url, params=params, headers=tmdb_headers).json()
+    return response["results"]
 
 
 @app.route("/")
@@ -78,6 +96,36 @@ def delete():
         db.session.delete(to_del_movie)
         db.session.commit()
     return redirect(url_for('home'))
+
+
+@app.route("/add", methods=["POST", "GET"])
+def add():
+    """For adding new movies into the database"""
+    add_form = AddMovieForm()
+    if add_form.validate_on_submit():
+        movie_title = add_form.title.data
+        movies_info = get_movie_info(movie_title)
+        return render_template("select.html", movies=movies_info)
+        # return redirect(url_for('home'))
+    return render_template("add.html", form=add_form)
+
+
+@app.route("/select", methods=["POST", "GET"])
+def select():
+    """This func gets the information of the movie, user selected in /add and creates a new entry in DB. The information is requested form TMDB"""
+    movie_id = request.args["id"]
+    api_url = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    response = requests.get(api_url, headers=tmdb_headers).json()
+    title = response["title"]
+    img_url = f"https://image.tmdb.org/t/p/w500{response['poster_path']}"
+    year = response["release_date"].split("-")[0]
+    description = response["overview"]
+    # Instance of Movie
+    new_movie = Movie(title=title, year=year, description=description, img_url=img_url, rating=0, review="Null", ranking=0)
+    # Creating a new entry in DB
+    with app.app_context():
+        db.session.add(new_movie)
+        db.session.commit()
 
 
 if __name__ == '__main__':
