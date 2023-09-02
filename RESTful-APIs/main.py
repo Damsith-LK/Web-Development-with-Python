7a9f125d4c33098abf826b6223e9f8343d23ee03
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-09-02 17:48:06 +0530

    Create a /search route to search for cafes at a particular location

--- RESTful-APIs/main.py
+++ RESTful-APIs/main.py
@@ -5,8 +5,9 @@ There are two requirements for an API to be RESTful:
     2. Use specific patterns of routes/endpoint URLs
 """
 """
-Challenge 1 - create a /random route that serves up a random cafe.
-Challenge 2 - create a /all route that serves up all the cafes.
+Challenge 1 - create a /random route that serves up a random cafe. - Done
+Challenge 2 - create a /all route that serves up all the cafes. - Done
+Challenge 3 - create a /search route to search for cafes at a particular location. - Done
 """
 
 from flask import Flask, jsonify, render_template, request, url_for, redirect
@@ -36,6 +37,24 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
+def format_json(id, name, map_url, img_url, loc, seats, has_toilet, has_wifi, has_sockets, can_take_calls, coffee_price):
+    """Format jsons"""
+    formatted_dict = {
+        "id": id,
+        "name": name,
+        "map_url": map_url,
+        "img_url": img_url,
+        "location": loc,
+        "seats": seats,
+        "has_toilet": has_toilet,
+        "has_wifi": has_wifi,
+        "has_sockets": has_sockets,
+        "can_take_calls": can_take_calls,
+        "coffee_price": coffee_price
+    }
+    return formatted_dict
+
+
 with app.app_context():
     db.create_all()
 
@@ -51,19 +70,8 @@ def random():
     rand_cafe = rand_lib.choice(cafes)
     # Serialization - Turning into JSON
     return jsonify(
-        cafe={
-            "id": rand_cafe.id,
-            "name": rand_cafe.name,
-            "map_url": rand_cafe.map_url,
-            "img_url": rand_cafe.img_url,
-            "location": rand_cafe.location,
-            "seats": rand_cafe.seats,
-            "has_toilet": rand_cafe.has_toilet,
-            "has_wifi": rand_cafe.has_wifi,
-            "has_sockets": rand_cafe.has_sockets,
-            "can_take_calls": rand_cafe.can_take_calls,
-            "coffee_price": rand_cafe.coffee_price
-        }
+        cafe=format_json(rand_cafe.id, rand_cafe.name, rand_cafe.map_url, rand_cafe.img_url, rand_cafe.location, rand_cafe.seats,
+                         rand_cafe.has_toilet, rand_cafe.has_wifi, rand_cafe.has_sockets, rand_cafe.can_take_calls, rand_cafe.coffee_price)
     )
 
 @app.route("/all")
@@ -74,21 +82,29 @@ def all():
     all_cafes = []
     # Iterating over each row in DB
     for cafe in cafes:
-        cafe_dict = {
-            "id": cafe.id,
-            "name": cafe.name,
-            "map_url": cafe.map_url,
-            "img_url": cafe.img_url,
-            "location": cafe.location,
-            "seats": cafe.seats,
-            "has_toilet": cafe.has_toilet,
-            "has_wifi": cafe.has_wifi,
-            "has_sockets": cafe.has_sockets,
-            "can_take_calls": cafe.can_take_calls,
-            "coffee_price": cafe.coffee_price
-        }
+        cafe_dict = format_json(cafe.id, cafe.name, cafe.map_url, cafe.img_url, cafe.location, cafe.seats, cafe.has_toilet, cafe.has_wifi, cafe.has_sockets, cafe.can_take_calls, cafe.coffee_price)
         all_cafes.append(cafe_dict)
     return jsonify(cafes=all_cafes)
+
+
+@app.route("/search")
+def search():
+    """Search cafes by location"""
+    loc = request.args["loc"]
+    cafes = db.session.execute(db.select(Cafe).where(Cafe.location == loc.title())).scalars().all()
+    # If there are cafes in the given location
+    if len(cafes) > 0:
+        search_cafes = []
+        for cafe in cafes:
+            cafe_dict = format_json(cafe.id, cafe.name, cafe.map_url, cafe.img_url, cafe.location, cafe.seats, cafe.has_toilet, cafe.has_wifi, cafe.has_sockets, cafe.can_take_calls, cafe.coffee_price)
+            search_cafes.append(cafe_dict)
+        return jsonify(search=search_cafes)
+    else:
+        return jsonify(
+            error={
+                "Not Found": "Sorry, we don't have a cafe at that location."
+            }
+        )
 
 
 if __name__ == '__main__':
