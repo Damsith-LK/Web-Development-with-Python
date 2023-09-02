7ac6243963bb17dbe7c7a7c165fe1f4728c50f9d
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-09-02 16:59:22 +0530

    Complete challenge 1 - create a /random route that serves up a random cafe

--- RESTful-APIs/main.py
+++ RESTful-APIs/main.py
@@ -4,19 +4,23 @@ There are two requirements for an API to be RESTful:
     1. Use HTTP verbs eg-GET
     2. Use specific patterns of routes/endpoint URLs
 """
+"""
+Challenge 1 - create a /random route that serves up a random cafe.
+"""
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+import random as rand_lib
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db = SQLAlchemy()
 db.init_app(app)
 
 
-##Cafe TABLE Configuration
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -38,7 +42,28 @@ with app.app_context():
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
+@app.route("/random")
+def random():
+    cafes = db.session.execute(db.select(Cafe)).scalars().all()
+    rand_cafe = rand_lib.choice(cafes)
+    # Serialization - Turning into JSON
+    return jsonify(
+        cafe={
+            "id": rand_cafe.id,
+            "name": rand_cafe.name,
+            "map_url": rand_cafe.map_url,
+            "img_url": rand_cafe.img_url,
+            "location": rand_cafe.location,
+            "seats": rand_cafe.seats,
+            "has_toilet": rand_cafe.has_toilet,
+            "has_wifi": rand_cafe.has_wifi,
+            "has_sockets": rand_cafe.has_sockets,
+            "can_take_calls": rand_cafe.can_take_calls,
+            "coffee_price": rand_cafe.coffee_price
+        }
+    )
+
 
 ## HTTP GET - Read Record
 
