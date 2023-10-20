85beb3273ad9021106f3352f1366673bd6fd16f9
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-10-20 14:45:01 +0530

    Now users can actually register and log in
    /secrets and /download are secured routes so only users who are logged in can access them

--- Flask-Authentication/main.py
+++ Flask-Authentication/main.py
@@ -13,9 +13,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy()
 db.init_app(app)
 
+# Configure LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Create a user_loader callback
+@login_manager.user_loader
+def load_user(user_id):
+    return db.get_or_404(User, user_id)
 
 # CREATE TABLE IN DB
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
@@ -45,7 +53,7 @@ def register():
             return redirect(url_for("login"))
         # Hashing and salting the password. This makes it super secure
         password = generate_password_hash(password=password, method="pbkdf2:sha256", salt_length=8)
-        # Creating a new entry in DB
+        # Creating a new entry in DB (In PyCharm, error shows when trying to create a new object from User class. But nothing goes wrong upon running the code)
         new_user = User(
             email=email,
             password=password,
@@ -53,6 +61,10 @@ def register():
         )
         db.session.add(new_user)
         db.session.commit()
+
+        # Log in the user
+        login_user(new_user)
+
         return redirect(url_for("secrets", name=name))
     return render_template("register.html")
 
@@ -67,16 +79,22 @@ def login():
         check_email = db.session.execute(db.select(User).where(User.email == email)).scalar()
         if not check_email:
             flash("This email does not exist. Please try again.", "error")
+        # Below else statement gets triggered if the inputted email exists in the DB
         else:
             # Check if the given password is correct. (Compare password hash and the inputted password)
             check_password = check_password_hash(check_email.password, password)
             if not check_password:
                 flash("Password incorrect. Please try again.", "error")
+            else:
+                # This else statement gets triggered if the user logged in with no errors
+                login_user(check_email, remember=True)
+
 
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
     name = request.args.get("name")
     return render_template("secrets.html", name=name)
@@ -84,10 +102,12 @@ def secrets():
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
+@login_required
 def download():
     """Download the cheat sheet to user's device"""
     return send_from_directory(
