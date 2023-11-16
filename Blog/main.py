7b697b56908bb51ac8f7022f4c18d02da2da0da8
Damsith Wijekoon <damsith.wijekoon@Outlook.com>
2023-11-16 13:10:53 +0530

    Start Day 69

--- Blog/main.py
+++ Blog/main.py
@@ -1,16 +1,26 @@
-import datetime
-from flask import Flask, render_template, redirect, url_for
+# Day 69 - Blog capstone finale
+
+from datetime import date
+from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_gravatar import Gravatar
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, URL, Length
-from flask_ckeditor import CKEditor, CKEditorField
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import relationship
+# Import your forms from the forms.py
+from forms import CreatePostForm
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-bootstrap = Bootstrap5(app)
 ckeditor = CKEditor(app)
+Bootstrap5(app)
+
+# TODO: Configure Flask-Login
+
 
 # CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
@@ -18,8 +28,9 @@ db = SQLAlchemy()
 db.init_app(app)
 
 
-# CONFIGURE TABLE
+# CONFIGURE TABLES
 class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
@@ -28,90 +39,95 @@ class BlogPost(db.Model):
     author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
 
-# CONFIGURE WTFORM
-class NewPostForm(FlaskForm):
-    """Form for creating new blog posts.
-    Use in add_new_post() and edit_post()"""
-    title = StringField(label="Blog Post Title", validators=[DataRequired(), Length(max=60)])
-    subtitle = StringField(label="Subtitle", validators=[DataRequired()])
-    author = StringField(label="Your Name", validators=[DataRequired()])
-    img_url = StringField(label="Blog Image URL", validators=[DataRequired(), URL()])
-    body = CKEditorField(label="Blog Content", validators=[DataRequired()])
-    submit = SubmitField(label="SUBMIT POST", render_kw={'style': 'margin: 15px;'})
+
+# TODO: Create a User table for all your registered users. 
+
 
 with app.app_context():
     db.create_all()
 
 
+# TODO: Use Werkzeug to hash the user's password when creating a new user.
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+# TODO: Retrieve a user from the database based on their email. 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('get_all_posts'))
+
+
 @app.route('/')
 def get_all_posts():
-    # Query the database for all the posts. Convert the data to a python list
-    posts = db.session.execute(db.select(BlogPost).order_by(BlogPost.id)).scalars().all()
+    result = db.session.execute(db.select(BlogPost))
+    posts = result.scalars().all()
     return render_template("index.html", all_posts=posts)
 
-# Add a route so that you can click on individual posts
-@app.route('/post/<int:post_id>')
+
+# TODO: Allow logged-in users to comment on posts
+@app.route("/post/<int:post_id>")
 def show_post(post_id):
-    # Retrieve a BlogPost from the database based on the post_id
     requested_post = db.get_or_404(BlogPost, post_id)
     return render_template("post.html", post=requested_post)
 
 
-# add_new_post() to create a new blog post
-@app.route("/new-post", methods=["POST", "GET"])
+# TODO: Use a decorator so only an admin user can create a new post
+@app.route("/new-post", methods=["GET", "POST"])
 def add_new_post():
-    new_blog_post_form = NewPostForm()
-    if new_blog_post_form.validate_on_submit():
-        title = new_blog_post_form.title.data
-        subtitle = new_blog_post_form.subtitle.data
-        author = new_blog_post_form.author.data
-        img_url = new_blog_post_form.img_url.data
-        body = new_blog_post_form.body.data
-        date = datetime.datetime.now().strftime('%B %d, %Y')
-        # Creating new entry in DB
+    form = CreatePostForm()
+    if form.validate_on_submit():
         new_post = BlogPost(
-            title=title,
-            subtitle=subtitle,
-            author=author,
-            img_url=img_url,
-            body=body,
-            date=date
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=current_user,
+            date=date.today().strftime("%B %d, %Y")
         )
         db.session.add(new_post)
         db.session.commit()
-        return redirect(url_for('get_all_posts'))
-    return render_template("make-post.html", form=new_blog_post_form, is_edit=False)
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form)
 
-# edit_post() to change an existing blog post
+
+# TODO: Use a decorator so only an admin user can edit a post
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
 def edit_post(post_id):
-    edit_post = db.get_or_404(BlogPost, post_id)
-    edit_blog_post_form = NewPostForm(
-        title=edit_post.title,
-        subtitle=edit_post.subtitle,
-        author=edit_post.author,
-        img_url=edit_post.img_url,
-        body=edit_post.body,
+    post = db.get_or_404(BlogPost, post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
     )
-    if edit_blog_post_form.validate_on_submit():
-        edit_post.title = edit_blog_post_form.title.data
-        edit_post.subtitle = edit_blog_post_form.subtitle.data
-        edit_post.author = edit_blog_post_form.author.data
-        edit_post.img_url = edit_blog_post_form.img_url.data
-        edit_post.body = edit_blog_post_form.body.data
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = current_user
+        post.body = edit_form.body.data
         db.session.commit()
-        return redirect(f"/post/{post_id}")
-    return render_template('make-post.html', is_edit=True, form=edit_blog_post_form)
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
 
-# delete_post() to remove a blog post from the database
-@app.route('/delete-post/<int:post_id>')
+
+# TODO: Use a decorator so only an admin user can delete a post
+@app.route("/delete/<int:post_id>")
 def delete_post(post_id):
-    del_post = db.get_or_404(BlogPost, post_id)
-    db.session.delete(del_post)
+    post_to_delete = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post_to_delete)
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
-# Below is the code from previous lessons. No changes needed.
+
 @app.route("/about")
 def about():
     return render_template("about.html")
@@ -123,4 +139,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    app.run(debug=True, port=5002)
